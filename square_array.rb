c60c03019fa6d6f3064903f3ddccdf6b461d6532
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-28 15:21:00 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(root_array)
+def square_array(array)
 root_array = [] 
 squared_array = []
-root_array.each do |root|
+array.each do |root|
   squared_array << (root**2) 
 end
 squared_array
